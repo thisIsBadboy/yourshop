b6db6765f563cb62a4956457af2a47b6f2b0a534
Rasel Ahammed <raselcse20@gmail.com>
2017-07-03 22:54:55 +0600

    change

--- resources/views/product/product-list.blade.php
+++ resources/views/product/product-list.blade.php
@@ -9,13 +9,48 @@
                 <h1 class="page-header">Products</h1>
 
                 @forelse($products as $product)
-                    <li>{{ $product['title'] }}</li>
-                    {{ Form::open(['method'=>'POST', 'route'=>['business.cart.store', $business]]) }}
+                    <div class="col-md-4">
+                        <div class="thumbnail">
+                            <img src="http://placehold.it/700x400" alt="" class="img-responsive">
 
-                        <input type="hidden" name="form[product_id]" value="{{ $product['id'] }}"/>
+                            <div class="caption">
+                              <h4 class="pull-right">TK {{ $product['price'] }}</h4>
+                              <h4><a href="#">{{ $product['title'] }}</a></h4>
+                              <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+                            </div>
 
-                        <input type="submit" class="btn btn-sm btn-success" value="Add to cart"/>
-                    {{ Form::close() }}
+                            {{--
+                            <div class="ratings">
+                              <p>
+                                <span class="glyphicon glyphicon-star"></span>
+                                <span class="glyphicon glyphicon-star"></span>
+                                <span class="glyphicon glyphicon-star"></span>
+                                <span class="glyphicon glyphicon-star"></span>
+                                <span class="glyphicon glyphicon-star"></span>
+                                 (15 reviews)
+                              </p>
+                            </div>
+                            --}}
+
+                            <div class="btn-ground text-center">
+                                <div class="row">
+                                    <div class="col-lg-6">
+                                    {{ Form::open(['method'=>'POST', 'route'=>['business.cart.store', $business]]) }}
+
+                                        <input type="hidden" name="form[product_id]" value="{{ $product['id'] }}"/>
+
+                                        <button type="submit" class="btn btn-primary"><i class="fa fa-shopping-cart"></i> Add To Cart</button>
+                                    {{ Form::close() }}
+                                    </div>
+
+                                    <div class="col-lg-6">
+                                        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#product_view"><i class="fa fa-search"></i> Quick View</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    
                 @empty
                     <p>Your business has no product!</p>
                 @endforelse
--- resources/views/sale-cart.blade.php
+++ resources/views/sale-cart.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <!-- Page Content -->
 <div id="page-wrapper">
-    <div class="container-fluid">
+    <div class="container-fluid" style="margin-bottom:20px;">
 
         {{ Form::open(['method'=>'POST', 'route'=>['business.sale_invoice.store', $business]]) }}
 
@@ -11,23 +11,53 @@
             <div class="col-lg-12">
                 <h1 class="page-header">Sale Cart</h1>
 
-                @forelse($sale_cart['contents'] as $content)
-                    <li>{{ $content['title'] }} ({{ $content['qty'] }}) -- TK {{ $content['subtotal'] }}</li>
-                @empty
-                    <p>Your sale cart has no product!</p>
-                @endforelse
+                <div class="panel panel-default">
+                    <div class="panel-body">
+                        <div class="table-responsive">
+                            <table class="table">
+                                <thead>
+                                    <tr>
+                                        <th>Image</th>
+                                        <th>Title</th>
+                                        <th style="text-align:right;">Quantity</th>
+                                        <th style="text-align:right;">Sub total</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                @foreach($sale_cart['contents'] as $content)
+                                    <tr>
+                                        <td><img src="http://placehold.it/700x400" alt="" class="img-responsive" width="200" height="150"></td>
+                                        <td>{{ $content['title'] }}</td>
+                                        <td style="text-align:right;">{{ $content['qty'] }}</td>
+                                        <td style="text-align:right;">{{ $content['subtotal'] }}</td>
+                                    </tr>
+                                @endforeach
 
-                <p>Total = {{ $sale_cart['total_amount'] }}</p>
+                                <tr>
+                                    <td></td>
+                                    <td></td>
+                                    <td style="text-align:right;">Total Quantity = {{ $sale_cart['total_qty'] }}</td>
+                                    <td style="text-align:right;">Total Amount = {{ $sale_cart['total_amount'] }}</td>
+                                </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+
+                </div>
+                
             </div>
             <!-- /.col-lg-12 -->
 
-            <div class="form-group">
-                <label>Paid Amount: </label>
-                <input class="form-control" placeholder="Paid Amount" name="form[paid_amount]" type="text" >
-                <p>{{ $errors->first('paid_amount') }}</p>
-            </div>
+            <div class="col-lg-4 pull-right">
+                <div class="form-group">
+                    <label>Paid Amount: </label>
+                    <input class="form-control" placeholder="Paid Amount" name="form[paid_amount]" type="text" >
+                    <p>{{ $errors->first('paid_amount') }}</p>
+                </div>
 
-            <input type="submit" class="btn btn-lg btn-success btn-block" value="Create Invoice"/>
+                <input type="submit" class="btn btn-lg btn-success btn-block" value="Create Invoice"/>
+            </div>
         </div>
         <!-- /.row -->
 
