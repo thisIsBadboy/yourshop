f8e58754426f343d4796be2123d3344e86cfb7a0
Rasel Ahammed <raselcse20@gmail.com>
2017-07-19 20:56:12 +0600

    change

--- resources/views/accounting/account-configuration.blade.php
+++ resources/views/accounting/account-configuration.blade.php
@@ -8,65 +8,81 @@
             <div class="col-lg-12">
                 <h1 class="page-header">Account Configuration</h1>
 
-                {{ Form::open(['method'=>'PUT', 'route'=>['business.account_configuration.update', $business, $account_settings['sale_cash_dr']['configuration_id'] ]]) }}
+                <div class="row">
 
-                <div class="form-group">
-                    <label>Debit Cash Account </label>
-                    <select class="form-control" name="form[account_id]">
-                    @foreach($chart_of_accounts as $account)
-                        <option value="{{ $account['id'] }}" @if($account_settings['sale_cash_dr']['account_id'] == $account['id']) selected @endif>{{ $account['name'] }} ({{ $account['code'] }})</option>
-                    @endforeach
-                    </select>
+                    <div class="col-lg-6" style="margin-bottom: 20px;">
+                    {{ Form::open(['method'=>'PUT', 'route'=>['business.account_configuration.update', $business, $account_settings['sale_cash_dr']['configuration_id'] ]]) }}
+
+                    <div class="form-group">
+                        <label>Debit Cash Account </label>
+                        <select class="form-control" name="form[account_id]">
+                        @foreach($chart_of_accounts as $account)
+                            <option value="{{ $account['id'] }}" @if($account_settings['sale_cash_dr']['account_id'] == $account['id']) selected @endif>{{ $account['name'] }} ({{ $account['code'] }})</option>
+                        @endforeach
+                        </select>
+                    </div>
+
+                    <input type="submit" class="btn btn-md btn-success btn-block" value="Update"/>
+
+                    {{ Form::close() }}
+                    </div>
+
+                    <div class="col-lg-6" style="margin-bottom: 20px;">
+                    {{ Form::open(['method'=>'PUT', 'route'=>['business.account_configuration.update', $business, $account_settings['sale_cash_cr']['configuration_id'] ]]) }}
+
+                    <div class="form-group">
+                        <label>Credit Cash Account </label>
+                        <select class="form-control" name="form[account_id]">
+                        @foreach($chart_of_accounts as $account)
+                            <option value="{{ $account['id'] }}" @if($account_settings['sale_cash_cr']['account_id'] == $account['id']) selected @endif>{{ $account['name'] }} ({{ $account['code'] }})</option>
+                        @endforeach
+                        </select>
+                    </div>
+
+                    <input type="submit" class="btn btn-md btn-success btn-block" value="Update"/>
+
+                    {{ Form::close() }}
+                    </div>
+
                 </div>
 
-                <input type="submit" class="btn btn-md btn-success btn-block" value="Update"/>
+                <div class="row">
 
-                {{ Form::close() }}
+                    <div class="col-lg-6" style="margin-bottom: 20px;">
+                    {{ Form::open(['method'=>'PUT', 'route'=>['business.account_configuration.update', $business, $account_settings['sale_due_dr']['configuration_id'] ]]) }}
 
-                {{ Form::open(['method'=>'PUT', 'route'=>['business.account_configuration.update', $business, $account_settings['sale_cash_cr']['configuration_id'] ]]) }}
+                    <div class="form-group">
+                        <label>Debit Due Account </label>
+                        <select class="form-control" name="form[account_id]">
+                        @foreach($chart_of_accounts as $account)
+                            <option value="{{ $account['id'] }}" @if($account_settings['sale_due_dr']['account_id'] == $account['id']) selected @endif>{{ $account['name'] }} ({{ $account['code'] }})</option>
+                        @endforeach
+                        </select>
+                    </div>
 
-                <div class="form-group">
-                    <label>Credit Cash Account </label>
-                    <select class="form-control" name="form[account_id]">
-                    @foreach($chart_of_accounts as $account)
-                        <option value="{{ $account['id'] }}" @if($account_settings['sale_cash_cr']['account_id'] == $account['id']) selected @endif>{{ $account['name'] }} ({{ $account['code'] }})</option>
-                    @endforeach
-                    </select>
+                    <input type="submit" class="btn btn-md btn-success btn-block" value="Update"/>
+
+                    {{ Form::close() }}
+                    </div>
+
+                    <div class="col-lg-6" style="margin-bottom: 20px;">
+                    {{ Form::open(['method'=>'PUT', 'route'=>['business.account_configuration.update', $business, $account_settings['sale_due_cr']['configuration_id'] ]]) }}
+
+                    <div class="form-group">
+                        <label>Credit Due Account </label>
+                        <select class="form-control" name="form[account_id]">
+                        @foreach($chart_of_accounts as $account)
+                            <option value="{{ $account['id'] }}" @if($account_settings['sale_due_cr']['account_id'] == $account['id']) selected @endif>{{ $account['name'] }} ({{ $account['code'] }})</option>
+                        @endforeach
+                        </select>
+                    </div>
+
+                    <input type="submit" class="btn btn-md btn-success btn-block" value="Update"/>
+
+                    {{ Form::close() }}
+                    </div>
+
                 </div>
-
-                <input type="submit" class="btn btn-md btn-success btn-block" value="Update"/>
-
-                {{ Form::close() }}
-
-                {{ Form::open(['method'=>'PUT', 'route'=>['business.account_configuration.update', $business, $account_settings['sale_due_dr']['configuration_id'] ]]) }}
-
-                <div class="form-group">
-                    <label>Debit Due Account </label>
-                    <select class="form-control" name="form[account_id]">
-                    @foreach($chart_of_accounts as $account)
-                        <option value="{{ $account['id'] }}" @if($account_settings['sale_due_dr']['account_id'] == $account['id']) selected @endif>{{ $account['name'] }} ({{ $account['code'] }})</option>
-                    @endforeach
-                    </select>
-                </div>
-
-                <input type="submit" class="btn btn-md btn-success btn-block" value="Update"/>
-
-                {{ Form::close() }}
-
-                {{ Form::open(['method'=>'PUT', 'route'=>['business.account_configuration.update', $business, $account_settings['sale_due_cr']['configuration_id'] ]]) }}
-
-                <div class="form-group">
-                    <label>Credit Due Account </label>
-                    <select class="form-control" name="form[account_id]">
-                    @foreach($chart_of_accounts as $account)
-                        <option value="{{ $account['id'] }}" @if($account_settings['sale_due_cr']['account_id'] == $account['id']) selected @endif>{{ $account['name'] }} ({{ $account['code'] }})</option>
-                    @endforeach
-                    </select>
-                </div>
-
-                <input type="submit" class="btn btn-md btn-success btn-block" value="Update"/>
-
-                {{ Form::close() }}
 
             </div>
             <!-- /.col-lg-12 -->
